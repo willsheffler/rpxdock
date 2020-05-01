8c0e1fe264616ce6a802aa231e2b8c7db1240077
Alena Khmelinskaia <akhmelin@uw.edu>
2020-05-01 16:39:26 -0700

    Small changes in arg usage and larger cart bounds

--- build/lib/rpxdock/app/dock.py
+++ build/lib/rpxdock/app/dock.py
@@ -69,12 +69,12 @@ def dock_onecomp(hscore, **kw):
    # double normal resolution, cuz why not?
    # mirrorlayer for 1comp xtals and stuff with P architectures
    if arg.docking_method == 'grid':
-      cart = np.arange(-200.5, 200.6)
-      ang = np.arange(-60, 60.1, 1)
+      cart = np.arange(-300.5, 300.6)
+      ang = np.arange(-180/int(spec.axis), 180/int(spec.axis)+0.1, 1)
       if arg.flip_components is None:
          sampler = rp.sampling.grid_sym_axis(cart, ang, axis = spec.axis)
       else:
-         sampler = rp.sampling.grid_sym_axis(cart, ang, axis = [0,0,1], flip = [1,0,0])
+         sampler = rp.sampling.grid_sym_axis(cart, ang, axis = spec.axis, flip = arg.flip_components)
       search = rp.grid_search
    else:
       if spec.type == 'mirrorlayer':
