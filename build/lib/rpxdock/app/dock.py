c5b8317f51ec80c222a15108d2bcf4ed89c0d5aa
Alena Khmelinskaia <akhmelin@uw.edu>
2020-05-01 16:54:44 -0700

    Axis for flip corrected

--- build/lib/rpxdock/app/dock.py
+++ build/lib/rpxdock/app/dock.py
@@ -70,11 +70,11 @@ def dock_onecomp(hscore, **kw):
    # mirrorlayer for 1comp xtals and stuff with P architectures
    if arg.docking_method == 'grid':
       cart = np.arange(-300.5, 300.6)
-      ang = np.arange(-180/int(arch[1]), 180/int(arch[1])+0.1, 1)
+      ang = np.arange(-180/int(arg.architecture[1]), 180/int(arg.architecture[1])+0.1, 1)
       if arg.flip_components is None:
          sampler = rp.sampling.grid_sym_axis(cart, ang, axis = spec.axis)
       else:
-         sampler = rp.sampling.grid_sym_axis(cart, ang, axis = spec.axis, flip = arg.flip_components)
+         sampler = rp.sampling.grid_sym_axis(cart, ang, axis = [0,0,1], flip = arg.flip_components)
       search = rp.grid_search
    else:
       if spec.type == 'mirrorlayer':
