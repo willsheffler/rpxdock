e7369fe750d0123955f655f3d2e81535c2bebac4
Alena Khmelinskaia <akhmelin@uw.edu>
2020-06-03 15:01:15 -0700

    Correct cart_bounds parsing

--- rpxdock/app/dock.py
+++ rpxdock/app/dock.py
@@ -67,7 +67,7 @@ def dock_onecomp(hscore, **kw):
    # double normal resolution, cuz why not?
    if kw.docking_method == 'grid':
       sampler = rp.sampling.grid_sym_axis(
-         cart=np.arange(kw.cart_bounds[0], kw.cart_bounds[1], kw.grid_resolution_cart_angstroms),
+         cart=np.arange(kw.cart_bounds[0][0], kw.cart_bounds[0][1], kw.grid_resolution_cart_angstroms),
          ang=np.arange(0, 360 / spec.nfold, kw.grid_resolution_ori_degrees), axis=spec.axis,
          flip=list(spec.flip_axis[:3]))
       search = rp.grid_search
