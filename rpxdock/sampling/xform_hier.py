Erinyang/fixed rotation (#28)
* added fixed_rot and fixed_comp stuff into most recent version of master

* Allowed fixed_wiggle to adjust by sampling resolution. Otherwise, it wasn't outputting docks for some cases.

* Added fixed_trans, changed defualts for fixed_wiggle, and added assert
for cart/ang nstep

* messing around with RotHier pickleability, still not working

* Resolved errors in fixed_comps (fixed_trans, fixed_rot, fixed_wiggle)

* 1. Removed proprietary pyrosetta credentials for public repository in
environment.yml file
2. Added descriptions to confusing flags in rpxdock/app/options.py
3. Fixed pytest errors

* Removed inputs and allowed_residues from help options

* Fixed scorefunction bug and added descriptions in options.py

* Edited dihedral symmetry definition in help for flags

* turned off resolution dependent scoring until we figure it out later

Co-authored-by: Erin Yang <erinyang@digs>
Co-authored-by: Erin Yang <erinyang@localhost>
Co-authored-by: Erin Yang <erinyang@digs.baker>

rpxdock/sampling/xform_hier.py
@@ -55,6 +55,16 @@ def hier_multi_axis_sampler(
    resl=10,
    angresl=10,
    flip_components=True,
+   fixed_rot=[],
+   fixed_components=[],
+   fixed_trans=[],
+   fixed_wiggle=[],
+   fw_cartlb=-5,
+   fw_cartub=5,
+   fw_rotlb=-5,
+   fw_rotub=5,
+   fw_cartnc=1,
+   fw_rotnc=1,
    **kw,
 ):
    if not (hasattr(spec, 'nfold') and hasattr(spec, 'axis') and hasattr(spec, 'xflip')):
@@ -73,14 +83,28 @@ def hier_multi_axis_sampler(
    cart_bounds = np.tile(cart_bounds, [8, 1])  # for wrapping / repeating
 
    cart_nstep = np.ceil((cart_bounds[:, 1] - cart_bounds[:, 0]) / resl).astype('i')
+   assert np.all(cart_nstep > 0)
 
    ang = 360 / spec.nfold
    ang_nstep = np.ceil(ang / angresl).astype('i')
-
+   assert np.all(ang_nstep > 0) 
+   
    samp = []
    for i in range(len(spec.nfold)):
       if spec.comp_is_dihedral[i]:
          s = LineHier(cart_bounds[i, 0], cart_bounds[i, 1], cart_nstep[i], spec.axis[i])
+      elif i in fixed_rot: 
+         s = LineHier(cart_bounds[i, 0], cart_bounds[i, 1], cart_nstep[i], spec.axis[i])
+      elif i in fixed_trans: 
+         s = rp.sampling.RotHier_f4(0, ang[i], ang_nstep[i], spec.axis[i][:3]) #TODO: MDL try this
+      elif i in fixed_components:
+         s = rp.ZeroDHier([np.eye(4)])
+      elif i in fixed_wiggle: #TODO: MDL try this
+         #Samples +/- 3 angstroms along sym axis, and same value around the symaxis
+         if fw_cartnc and fw_rotnc: 
+             s = rp.sampling.RotCart1Hier_f4(fw_cartlb,  fw_cartub, fw_cartnc, fw_rotlb, fw_rotub, fw_rotnc, spec.axis[i][:3])
+         else:
+             s =  rp.sampling.RotCart1Hier_f4(fw_cartlb,  fw_cartub, cart_nstep[i], fw_rotlb, fw_rotub, ang_nstep[i], spec.axis[i][:3])
       else:
          s = rp.sampling.RotCart1Hier_f4(cart_bounds[i, 0], cart_bounds[i, 1], cart_nstep[i], 0,
                                          ang[i], ang_nstep[i], spec.axis[i][:3])

rpxdock/score/score_functions.py
@@ -36,14 +36,14 @@ def sasa_priority(pos1, pos2, lbub, lbub1, lbub2, ressc1, ressc2, **kw):
    #TODO: Quinton: Resolution-dependent scoring is turned off while I try to optimize it further. This does nothing right now.
    if kw.wts.ncontact != 0:
       if kw.wts.rpx != 0:
-         #start_sasa = kw.wts.sasa + ( 576 * 4 )
-         #sasa = start_sasa - ( 576 * kw.iresl )
+      #   start_sasa = kw.wts.sasa + ( 576 * 4 )
+      #   sasa = start_sasa - ( 576 * kw.iresl )
          sasa = kw.wts.sasa
-         #if not kw.wts.error:
-         #   start_error = 6
-         #else:
-         #   start_error = kw.wts.error + 2
-         #sigma = start_error - ( kw.iresl / 2 )
+      #   if not kw.wts.error:
+      #      start_error = 6
+      #   else:
+      #      start_error = kw.wts.error + 2
+      #   sigma = start_error - kw.iresl 
          sigma = kw.wts.error
       else:
          if not kw.wts.error:
@@ -97,7 +97,7 @@ def stnd(pos1, pos2, lbub, lbub1, lbub2, ressc1, ressc2, **kw):
    for i, (lb, ub) in enumerate(lbub):
       side1 = np.sum(ressc1[lbub1[i, 0]:lbub1[i, 1]])
       side2 = np.sum(ressc2[lbub2[i, 0]:lbub2[i, 1]])
-      mscore = (side1 + side2) / 2
+      mscore = (side1 + side2) 
       scores[i] = kw.wts.rpx * mscore + kw.wts.ncontact * (ub - lb)
    return scores
   
@@ -127,7 +127,7 @@ def exp(pos1, pos2, lbub, lbub1, lbub2, ressc1, ressc2, **kw):
    for i, (lb, ub) in enumerate(lbub):
       side1 = np.sum(ressc1[lbub1[i, 0]:lbub1[i, 1]])
       side2 = np.sum(ressc2[lbub2[i, 0]:lbub2[i, 1]])
-      mscore = (side1 + side2) / 2
+      mscore = (side1 + side2)
       scores[i] = mscore - ( 4.6679 * ((ub - lb)**0.588  ))
    return scores
   
@@ -137,6 +137,6 @@ def lin(pos1, pos2, lbub, lbub1, lbub2, ressc1, ressc2, **kw):
    for i, (lb, ub) in enumerate(lbub):
       side1 = np.sum(ressc1[lbub1[i, 0]:lbub1[i, 1]])
       side2 = np.sum(ressc2[lbub2[i, 0]:lbub2[i, 1]])
-      mscore = (side1 + side2) / 2
+      mscore = (side1 + side2)
       scores[i] = mscore - ( 0.7514*(ub - lb) )
    return scores