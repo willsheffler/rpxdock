0149ec82cc4c7cc5c427b426eed18f31e83577b7
Alena Khmelinskaia <akhmelin@uw.edu>
2021-10-14 09:13:47 -0700

    Fix tests axel

--- rpxdock/tests/search/test_axel.py
+++ rpxdock/tests/search/test_axel.py
@@ -25,7 +25,7 @@ def test_axel(hscore):
    kw.beam_size = 5000
    spec = rp.search.DockSpecAxel("AXEL_3")
 
-   sampler1 = rp.sampling.hier_axis_sampler(3, 0, 100, 10, 10)
+   sampler1 = rp.sampling.hier_axis_sampler(3, 0, 100, 10, 10, axis=spec.axis[0], flipax=spec.flip_axis[0])
    sampler2 = rp.sampling.ZeroDHier([np.eye(4),rp.homog.hrot([1,0,0],180)])
    sampler = rp.sampling.CompoundHier(sampler2, sampler1)
    _,x = sampler.get_xforms(resl=0, idx=[0])
@@ -47,7 +47,7 @@ def test_axel_asym(hscore):
    spec = rp.search.DockSpecAxel("AXEL_1_2_3")
    
 
-   sampler1 = rp.sampling.hier_axis_sampler(2*3, 0, 100, 10, 10)
+   sampler1 = rp.sampling.hier_axis_sampler(2*3, 0, 100, 10, 10, axis=spec.axis[0], flipax=spec.flip_axis[0])
    sampler2 = rp.sampling.ZeroDHier([np.eye(4),rp.homog.hrot([1,0,0],180)])
    sampler = rp.sampling.CompoundHier(sampler2, sampler1)
    _,x = sampler.get_xforms(resl=0, idx=[0])
@@ -58,9 +58,11 @@ def test_axel_asym(hscore):
 #   result.dump_pdbs_top_score(hscore=hscore,
 #                               **kw.sub(nout_top=50, output_prefix='test_axel_asym'))
 
-def test_axel_grid(hscore, C3_A, C3_B):
-   C2_A = rp.data.get_body('C2_3hm4_1')
-   C2_A.init_coords(2, [0,0,1,0])
+def test_axel_grid(hscore):
+   C3_A = rp.data.get_body('C3_1na0-1_1')
+   C3_A.init_coords(3, [0,0,1,0])
+#   C2_A = rp.data.get_body('C2_3hm4_1')
+#   C2_A.init_coords(2, [0,0,1,0])
    C3_B = rp.data.get_body('C3_1nza_1')
    C3_B.init_coords(3, [0,0,1,0])
 
@@ -72,7 +74,7 @@ def test_axel_grid(hscore, C3_A, C3_B):
          cart=np.arange(0, 100, 2),
          ang=np.arange(0, 360 / 3, 2),
          axis=spec.axis[0],
-         flip=[0,1,0]
+         flip=list(spec.flip_axis[0])[:3],
          )
    shape = (2,2*len(sampler1),4,4)
    sampler = np.zeros(shape=shape)
