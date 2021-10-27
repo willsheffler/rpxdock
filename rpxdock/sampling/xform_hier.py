bbeceede417ab633f18a3c5cc6ab834f997dcc8b
Alena Khmelinskaia <akhmelin@uw.edu>
2021-10-27 10:52:19 -0700

    Pool request revision

--- rpxdock/sampling/xform_hier.py
+++ rpxdock/sampling/xform_hier.py
@@ -63,7 +63,7 @@ def hier_axis_sampler(
    else:
       samp = rp.sampling.RotCart1Hier_f4(lb, ub, cart_nstep, 0,
                                       ang, ang_nstep, axis[:3])
-   if kw.flip_components != None:
+   if kw.flip_components == [0]:
       flip = rp.ZeroDHier([np.eye(4), rp.homog.hrot(flipax, 180)])
       samp = rp.ProductHier(samp, flip)
    return samp
